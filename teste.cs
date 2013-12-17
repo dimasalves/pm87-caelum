72d239c15abf81f63fbfc1959e4c66a21ba7c189
Dimas Alves <dimasalves@gmail.com>
2013-12-17 21:18:06 -0200

    rebasing from master

--- teste.cs
+++ teste.cs
@@ -3,6 +3,7 @@ public class Teste
 	public static void Main(string[] args)
 	{
 		//comentando para criar uma brach
+		//fiz o outro comentario
 		Console.WriteLine("Exemplo de teste de uso do git");
 	}
 }
